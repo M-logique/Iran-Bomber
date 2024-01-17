57c88012389e335b434d619b618dfbe2948d2be0
MohsenFn <MaybeEvmo@gmail.com>
2024-01-17 14:33:40 +0330

    Add CLI texts to cli_texts.py

---
+++ cli_texts.py
@@ -0,0 +1,49 @@
+banner = '''
+
+
+
+            ██╗██████╗  █████╗ ███╗  ██╗
+            ██║██╔══██╗██╔══██╗████╗ ██║
+            ██║██████╔╝███████║██╔██╗██║
+            ██║██╔══██╗██╔══██║██║╚████║
+            ██║██║  ██║██║  ██║██║ ╚███║
+            ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝
+
+    ██████╗  █████╗ ███╗   ███╗██████╗ ███████╗██████╗ 
+    ██╔══██╗██╔══██╗████╗ ████║██╔══██╗██╔════╝██╔══██╗
+    ██████╦╝██║  ██║██╔████╔██║██████╦╝█████╗  ██████╔╝
+    ██╔══██╗██║  ██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗
+    ██████╦╝╚█████╔╝██║ ╚═╝ ██║██████╦╝███████╗██║  ██║
+    ╚═════╝  ╚════╝ ╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+    '''
+
+error_msg = '''
+            {2}Usage = {1}python bomber.py +989123456789{2}
+            self.proxy = {1}--self.proxy{2}
+            threads = {1}--threads {4}<amount>{2}
+            {4}>>{3} python bomber.py {1}--proxies --threads 7
+                {4}>>>{5} This will start bombing with proxies and 7 Threads 
+            {0}
+            Tip: 
+                You can use {4}<{0} python bomber.py --scrapp {4}>{0} to
+                get Fresh proxies! 
+                
+            {5}Press Enter 2 times for exit
+    '''
+
+
+successful_start_msg = '''
+        
+                            {5}Started the Job with {0}{6}{5} thread(s)                   
+                                                    
+                            {2}Phone Number {3}={0} {7}                          
+                                                    
+                            {4}Proxies:                                           
+                                {3}>>> {2}State{4}: {0}{8} {9}                        
+
+
+                    {11}       
+    '''
+
+
+updated_proxy_msg = "   Your 'proxies.txt' has been updated, Enjoy!",
